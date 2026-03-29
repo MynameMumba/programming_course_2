1fc242eaf9f6c1bb7339c3176d8091fb7f530412
MynameMumba <ivanlevichev20112007@gmail.com>
2026-03-29 21:31:07 +0700

    что-то там сделал

--- labs/lab13_linear_algebra/Lab13/src/main.cpp
+++ labs/lab13_linear_algebra/Lab13/src/main.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
 using namespace std;
-// Lab 13
-// TODO: реализуйте решение по заданию в labs/lab13_linear_algebra/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
 
 int main() {
-    int* D;
-    D = new int[3];
+    // чтение txt файла
+    string line;
+    ifstream in("Test.txt");
+    getline(in, line);
+
+    int M, N;
+    istringstream iss(line);
+    iss >> M;
+    iss >> N;
+
+    //создаём и заполняем матрицу
+    int **matrix = new int*[M];
+    for (int i = 0; i < M; i++) {
+        matrix[i] = new int[N]{};
+    }
+    for (int i = 0; i < M; i++) {
+        getline(in, line);
+        istringstream iss(line);
+        for (int j = 0; j < N; j++) {
+            iss >> matrix[i][j];
+        }
+    }
+    in.close();
+
+
+    // вывод результата в txt файл
+    ofstream out("Test.txt", ios::app);
+    out << "печенько" << endl;
+    // удаление матрицы
+    for (int i = 0; i < M; i++) {
+        delete [] matrix[i];
+    }
+    delete[] matrix;
     return 0;
 }
